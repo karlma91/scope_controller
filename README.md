8bb77c3e25596b9e6475294fe563787fcb811716
Karl <karl@domos.no>
2023-06-12 13:18:34 +0200

    fixed controlls

---
+++ README.md
@@ -0,0 +1,5 @@
+retrolink n64 usb controller uuid:
+03000000-7900-0000-0600-000010010000
+Controller sdl db line
+03000000790000000600000010010000,RetroLink N64,x:b3,a:b2,b:b1,y:b0,back:b8,start:b9,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,dpup:h0.1,leftshoulder:b4,lefttrigger:b6,rightshoulder:b5,righttrigger:b7,leftstick:b10,rightstick:b11,leftx:a0,lefty:a3,rightx:a1,righty:a4,platform:Linux,
+
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 use clap::{Arg, Command};
 use serialport::{ClearBuffer, DataBits, FlowControl, Parity, SerialPort, StopBits};
 use std::io::{self, Write};
+use uuid::Uuid;
 use std::io::BufReader;
 use std::io::BufRead;
 #[derive(Debug, Copy, Clone)]
@@ -20,7 +21,9 @@ enum Steppers{
 #[derive(Debug)]
 enum StepDir {
     UP,
-    DOWN
+    DOWN,
+    BIG_UP,
+    BIG_DOWN
 }
 
 fn move_stepper(port: &mut dyn serialport::SerialPort, st: Steppers ,sd: StepDir, sp: &mut StepPos){
@@ -30,33 +33,41 @@ fn move_stepper(port: &mut dyn serialport::SerialPort, st: Steppers ,sd: StepDir
         Steppers::YAxis => {
             match sd {
                 StepDir::DOWN => byte = "y".to_string(),
-                StepDir::UP => byte = "u".to_string()
+                StepDir::UP => byte = "u".to_string(),
+                StepDir::BIG_DOWN => byte = "r".to_string(),
+                StepDir::BIG_UP=> byte = "t".to_string(),
             }
         },
         Steppers::XAxis => {
             match sd {
                 StepDir::DOWN => byte = "b".to_string(),
-                StepDir::UP => byte = "a".to_string()
+                StepDir::UP => byte = "a".to_string(),
+                StepDir::BIG_DOWN => byte = "r".to_string(),
+                StepDir::BIG_UP=> byte = "t".to_string(),
             }
         },
         Steppers::Focus => {
             match sd {
-                StepDir::DOWN => byte = "F".to_string(),
-                StepDir::UP => byte = "G".to_string()
+                StepDir::DOWN => byte = "f".to_string(),
+                StepDir::UP => byte = "g".to_string(),
+                StepDir::BIG_DOWN => byte = "F".to_string(),
+                StepDir::BIG_UP=> byte = "G".to_string(),
             }
         }
     }
     println!("Writing: {}", byte);
     let output = byte.as_bytes();
-    port.write(output).expect("Write failed!");
-    thread::sleep(time::Duration::from_millis(100));
+    port.write(output);
+    thread::sleep(time::Duration::from_millis(10));
 
     let mut serial_buf: Vec<u8> = vec![0; 32];
-    port.read(serial_buf.as_mut_slice()).expect("Found no data!");
+    port.read(serial_buf.as_mut_slice());
     io::stdout().write_all(&serial_buf).unwrap();
     match sd {
         StepDir::DOWN => sp.pos -= 1,
-        StepDir::UP => sp.pos += 1
+        StepDir::UP => sp.pos += 1,
+        StepDir::BIG_DOWN => sp.pos -= 64,
+        StepDir::BIG_UP => sp.pos += 64
     }
     
     println!("{:?}:{:?} {}", st, sd, sp.pos);
@@ -73,6 +84,7 @@ fn main() {
     for p in ports {
         println!("{}", p.port_name);
         port_path = p.port_name;
+	break;
     }
 
     let mut port = match serialport::new(port_path, 9600).open() {
@@ -89,7 +101,7 @@ fn main() {
     port.flush().unwrap();
    
     println!("Reading from port");
-    port.set_timeout(Duration::from_millis(100));
+    port.set_timeout(Duration::from_millis(500));
     let mut serial_buf: Vec<u8> = Vec::new();
     match port.read_to_end(&mut serial_buf){
         Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
@@ -98,7 +110,8 @@ fn main() {
     }
     // Iterate over all connected gamepads
     for (_id, gamepad) in gilrs.gamepads() {
-        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+        let guuid = Uuid::from_bytes(gamepad.uuid());
+        println!("{} is {:?} {:?} {} ", gamepad.name(), gamepad.power_info(), gamepad.map_name(), guuid.urn());
     }
 
     let mut active_gamepad = None;
@@ -106,26 +119,35 @@ fn main() {
     loop {
         // Examine new events
         while let Some(Event { id, event, time }) = gilrs.next_event() {
-            // println!("{:?}", event);
+            println!("{:?}", event);
             active_gamepad = Some(id);
         }
         
         // You can also use cached gamepad state
         if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
-            }
             if gamepad.is_pressed(Button::DPadLeft) {
-                move_stepper(&mut *port, Steppers::XAxis, StepDir::DOWN, &mut stepp_ps[0])
+                move_stepper(&mut *port, Steppers::YAxis, StepDir::DOWN, &mut stepp_ps[0])
             }
             if gamepad.is_pressed(Button::DPadRight) {
-                move_stepper(&mut *port, Steppers::XAxis, StepDir::UP, &mut stepp_ps[0])
+                move_stepper(&mut *port, Steppers::YAxis, StepDir::UP, &mut stepp_ps[0])
             }
             if gamepad.is_pressed(Button::DPadDown) {
-                move_stepper(&mut *port, Steppers::YAxis, StepDir::DOWN, &mut stepp_ps[1])
+                move_stepper(&mut *port, Steppers::XAxis, StepDir::DOWN, &mut stepp_ps[1])
             }
             if gamepad.is_pressed(Button::DPadUp) {
-                move_stepper(&mut *port, Steppers::YAxis, StepDir::UP, &mut stepp_ps[1])
+                move_stepper(&mut *port, Steppers::XAxis, StepDir::UP, &mut stepp_ps[1])
+            }
+            if gamepad.is_pressed(Button::Select) {
+                move_stepper(&mut *port, Steppers::Focus, StepDir::DOWN, &mut stepp_ps[2])
+            }
+            if gamepad.is_pressed(Button::LeftTrigger2) {
+                move_stepper(&mut *port, Steppers::Focus, StepDir::UP, &mut stepp_ps[2])
+            }
+            if gamepad.is_pressed(Button::RightTrigger) {
+                move_stepper(&mut *port, Steppers::Focus, StepDir::BIG_DOWN, &mut stepp_ps[2])
+            }
+            if gamepad.is_pressed(Button::LeftTrigger) {
+                move_stepper(&mut *port, Steppers::Focus, StepDir::BIG_UP, &mut stepp_ps[2])
             }
         }
     }
